8c31e8652d50bd645d0b29f326faeb2d7b77217d
roodhouse <rughjm@gmail.com>
2023-07-16 19:41:31 -0500

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      white: '#ffffff',
+      dark: '#0B0D17',
+      lightBlue: '#D0D6F9'
+    }
+  },
+  plugins: [],
+}
