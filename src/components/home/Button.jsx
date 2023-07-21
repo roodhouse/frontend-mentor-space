f10cb86e41c6b6beac32bc774c2a7bf408e4b45b
roodhouse <rughjm@gmail.com>
2023-07-20 19:13:52 -0500

    done

--- src/components/home/Button.jsx
+++ src/components/home/Button.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion"
 function Button() {
   return (
     <>
-      <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} id="exploreButtonContainer" className='flex justify-center cursor-pointer xl:border-[88px] xl:border-[transparent] xl:hover:border-[88px] xl:hover:border-[rgba(255,255,255,0.5)] xl:hover:rounded-[50%]'>
+      <motion.div whileTap={{ scale: 0.9 }} id="exploreButtonContainer" className='flex justify-center cursor-pointer xl:border-[88px] xl:border-[transparent] xl:rounded-[50%] xl:hover:border-[88px] xl:hover:border-[rgba(255,255,255,0.5)] transition'>
         <div id="exploreButton" className='w-[150px] h-[150px] bg-white flex justify-center items-center rounded-[50%] md:w-[242px] md:h-[242px] xl:w-[274px] xl:h-[274px]'>
           <p className='text-dark text-center font-["Bellefair"] text-xl font-normal tracking-[1.25px] md:text-[32px] md:tracking-[2px] xl:text-[32px]'>EXPLORE</p>
         </div>
